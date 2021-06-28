2ac0bb988d721288db98ce646f1ec59753347baa
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-06-27 21:11:15 -0300

    matching strings

---
+++ functional_elixir/chapter3/matching_parts_of_strings.exs
@@ -0,0 +1,6 @@
+"Authentication: " <> credentials = "Authentication: Basic dXNlcjpwYXNz"
+# iex> credentials
+# "Basic dXNlcjpwYXNz"
+"eoD " <> first_name = String.reverse("John Doe")
+# iex> String.reverse(first_name)
+# "John"
